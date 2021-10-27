8b3b817fb16d944e7f40134711f022a8615b80ad
Kristof <you@example.com>
2021-10-27 12:07:50 +0200

    Betűváltás

--- app/src/main/java/com/example/akasztofa/MainActivity.java
+++ app/src/main/java/com/example/akasztofa/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.akasztofa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,16 +20,63 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewBetu, textViewSzo;
     private ImageView imageViewAkasztofa;
     private String[] szavak = new String[] {
-            "AUTÓ"/*, "HÁZ", "ANANÁSZ", "PROGRAMOZÁS", "TANÁR",
-            "PÉNZ", "LÁTOGATÓ", "ŰRLÉNY", "FASZÁLLÍTÓ", "XD"*/
+            "AUTÓ", "HÁZ", "ANANÁSZ", "PROGRAMOZÁS", "TANÁR",
+            "PÉNZ", "LÁTOGATÓ", "ŰRLÉNY", "FASZÁLLÍTÓ", "XD"
     };
     private String kitalalandoSzo, rejtettSzo;
+    private char[] abc;
+    private List<Character> tippeltBetuk;
+    private int betuIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        btnPlusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                betuValtas(true);
+            }
+        });
+
+        btnMinusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                betuValtas(false);
+            }
+        });
+    }
+
+    private void betuSzinezes(){
+        if (tippeltBetuk.contains(abc[betuIndex])){
+            textViewBetu.setTextColor(Color.parseColor("#000000"));
+        }
+        else{
+            textViewBetu.setTextColor(Color.parseColor("#CC0000"));
+        }
+    }
+
+    private void betuValtas(boolean elore){
+        if (elore){
+            if (betuIndex == abc.length - 1){
+                betuIndex = 0;
+            }
+            else{
+                betuIndex++;
+            }
+        }
+        else{
+            if (betuIndex == 0){
+                betuIndex = abc.length - 1;
+            }
+            else{
+                betuIndex--;
+            }
+        }
+        betuSzinezes();
+        textViewBetu.setText(abc[betuIndex] + "");
     }
 
     private void kirajzol(){
@@ -47,5 +101,8 @@ public class MainActivity extends AppCompatActivity {
         kitalalandoSzo = szavak[(int) (Math.random() * szavak.length)];
         rejtettSzo = "";
         kirajzol();
+        abc = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz".toUpperCase().toCharArray();
+        tippeltBetuk = new ArrayList<>();
+        betuIndex = 0;
     }
 }
